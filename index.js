d10cd3a0bcf64a61ba0657802947eb1480dcbd66
test <you@example.com>
2025-08-22 14:17:21 +0800

    Update

--- index.js
+++ index.js
@@ -81,7 +81,7 @@ const server = http.createServer(async (req, res) => {
     }
 
     res.writeHead(404, { 'Content-Type': 'text/plain; charset=utf-8' })
-    res.end('Not Found')
+    res.end('Not found')
 })
 
 server.listen(PORT, () => {
